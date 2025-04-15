6581ff324cfb4257432907ee701974ca34be03f0
dinesh-reddy-ys <dineshxyz07@gmail.com>
2025-04-15 07:01:31 +0530

    Added test cases for login screen orangehrm

---
+++ AutomationProject/src/main/java/com/orangehrm/pages/LoginPage.java
@@ -0,0 +1,64 @@
+package com.orangehrm.pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.automation.utils.WebDriverUtils;
+
+public class LoginPage {
+	private WebDriver driver;
+	private WebDriverWait wait;
+	
+	//Elements
+	@FindBy(name="username")
+	private WebElement usernameInput;
+	@FindBy(name = "password")
+	private WebElement passwordInput;
+	@FindBy(css=".oxd-button.oxd-button--medium.oxd-button--main.orangehrm-login-button")
+	private WebElement loginbutton;
+	@FindBy(css = ".oxd-icon.bi-exclamation-circle.oxd-alert-content-icon")
+	private WebElement invalidCredentials;
+	@FindBy(css=".oxd-text.oxd-text--span.oxd-input-field-error-message.oxd-input-group__message")
+	private WebElement requiredMessage;
+    
+	
+	public LoginPage() {
+		this.driver = WebDriverUtils.getDriver();
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		if(this.driver == null) {
+			throw new IllegalStateException("Driver is not initialized before calling HomePage constructor");
+		}
+		PageFactory.initElements(driver, this);	
+	}
+	
+	// Page action
+	public void enterUsername(String username) {
+		wait.until(ExpectedConditions.visibilityOf(usernameInput));		
+		usernameInput.clear();
+		usernameInput.sendKeys(username);
+	}
+	
+	public void enterPassword(String password) {
+		passwordInput.clear();
+		passwordInput.sendKeys(password);
+	}
+	public void clickLoginButton() {
+		loginbutton.click();
+	}
+	public void invalidCredentialsDisplayed() {
+		wait.until(ExpectedConditions.visibilityOf(invalidCredentials));
+		invalidCredentials.isDisplayed();
+	}
+	public void requiredMessageDisplayed() {
+		wait.until(ExpectedConditions.visibilityOf(requiredMessage));
+		requiredMessage.isDisplayed();
+	}
+	
+}
---
+++ AutomationProject/src/test/java/com/orangehrm/test/LoginTest.java
@@ -0,0 +1,78 @@
+package com.orangehrm.test;
+
+import java.time.Duration;
+
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseTest;
+import com.automation.utils.WebDriverUtils;
+import com.orangehrm.pages.HomePage;
+import com.orangehrm.pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+	
+	@Test(priority = 1)
+	public void testValidLogin() throws InterruptedException {
+		WebDriverUtils.getDriver().get("https://opensource-demo.orangehrmlive.com");
+		LoginPage login = new LoginPage();
+		login.enterUsername("admin");
+		login.enterPassword("admin123");
+		login.clickLoginButton();
+		HomePage home = new HomePage();
+		home.verifyElement();
+		Thread.sleep(Duration.ofSeconds(10));		
+	}
+	@Test(priority = 2)
+	public void testInvalidPassword() throws InterruptedException {
+		WebDriverUtils.getDriver().get("https://opensource-demo.orangehrmlive.com");
+		LoginPage login = new LoginPage();
+		login.enterUsername("admin");
+		login.enterPassword("admin124");
+		login.clickLoginButton();
+		login.invalidCredentialsDisplayed();
+		Thread.sleep(Duration.ofSeconds(10));
+	}
+	
+	@Test(priority = 3)
+	public void testInvalidUsername() {
+		WebDriverUtils.getDriver().get("https://opensource-demo.orangehrmlive.com");
+		LoginPage login = new LoginPage();
+		login.enterUsername("admin1");
+		login.enterPassword("admin123");
+		login.clickLoginButton();
+		login.invalidCredentialsDisplayed();
+		
+	}
+	@Test(priority = 4)
+	public void testEmptyPasswordField() {
+		WebDriverUtils.getDriver().get("https://opensource-demo.orangehrmlive.com");
+		LoginPage login = new LoginPage();
+		login.enterUsername("admin");
+		login.enterPassword("");
+		login.clickLoginButton();
+		login.requiredMessageDisplayed();
+	}
+	
+	@Test(priority = 5)
+	public void testEmptyUsername() {
+		WebDriverUtils.getDriver().get("https://opensource-demo.orangehrmlive.com");
+		LoginPage login = new LoginPage();
+		login.enterUsername("");
+		login.enterPassword("admin123");
+		login.clickLoginButton();
+		login.requiredMessageDisplayed();
+		
+	}
+	
+	@Test(priority = 6)
+	public void testEmptyCredentials() {
+		WebDriverUtils.getDriver().get("https://opensource-demo.orangehrmlive.com");
+		LoginPage login = new LoginPage();
+		login.enterUsername("");
+		login.enterPassword("");
+		login.clickLoginButton();
+		login.requiredMessageDisplayed();
+	}
+	
+
+}
