02fa7e845639a065bb39bcd1c8ae9a22a99e4f96
dinesh-reddy-ys <dineshxyz07@gmail.com>
2025-05-14 20:20:15 +0530

    removed references

--- AutomationProject/src/main/java/mobile/utils/MobileDriverUtils.java
+++ AutomationProject/src/main/java/mobile/utils/MobileDriverUtils.java
@@ -16,8 +16,8 @@ public class MobileDriverUtils {
         caps.setCapability("platformName", "Android"); // Specify the platform as Android
         caps.setCapability("deviceName", "emulator-5554"); // Specify the device name
         caps.setCapability("app", "C:\\Users\\Cheth\\OneDrive\\Documents\\Mobile Automation\\artifact-android (1322)\\artifact-android-Signed.apk"); // Path to the app
-        caps.setCapability("appPackage", "com.nov.mobilert"); // App package name
-        caps.setCapability("appActivity", "crc64c15af62489e8d86f.MainActivity"); // App activity to launch
+        caps.setCapability("appPackage", ""); // App package name
+        caps.setCapability("appActivity", ""); // App activity to launch
 
         // Initialize the AppiumDriver with the Appium server URL and capabilities
         AppiumDriver driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);        
